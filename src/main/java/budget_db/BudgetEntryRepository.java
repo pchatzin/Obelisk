fc28ac917b5d15609f39813f588c8f2d44e8adcf
Pinelopi Chatzinikolaou <t8240167@aueb.gr>
2025-11-07 19:02:51 +0200

    Commit αλλαγών αλλά κάπου κολλάει γιαυτό δεν έχει γίνει push

--- src/main/java/budget_db/BudgetEntryRepository.java
+++ src/main/java/budget_db/BudgetEntryRepository.java
@@ -1,3 +1,5 @@
+package budget_db;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface BudgetEntryRepository extends JpaRepository<BudgetEntry, Long> {}
--- src/main/java/budget_db/BudgetImporter.java
+++ src/main/java/budget_db/BudgetImporter.java
@@ -1,3 +1,5 @@
+package budget_db;
+
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
