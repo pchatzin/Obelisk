e1f16b049f5dd1fff0d437cf4f5a4f32c11b94c0
Pinelopi Chatzinikolaou <t8240167@aueb.gr>
2025-11-13 19:40:56 +0200

    last changes

--- src/main/java/com/obelisk/budget_db/BudgetEntry.java
+++ src/main/java/com/obelisk/budget_db/BudgetEntry.java
@@ -1,7 +1,8 @@
 package com.obelisk.budget_db;
 
-import jakarta.persistence.*;
 import java.math.BigDecimal;
+
+import jakarta.persistence.*;
 
 @Entity
 @Table(name = "budget_entries")
@@ -11,30 +12,17 @@ public class BudgetEntry {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    // 1η στήλη: αριθμός γραμμής στο CSV
     private Integer lineNumber;
-
-    // 2η στήλη: τύπος ("Εσοδα" ή "Εξοδα")
     private String type;
-
-    // 3η στήλη: ποσό
     private BigDecimal amount;
-
-    // 4η στήλη: υπουργείο
     private String ministry;
 
-    // 5η στήλη: από που προέρχεται / με τι σχετίζεται (ΦΠΑ, ΕΦΚ κλπ)
     @Column(length = 2000)
     private String source;
 
-    public BudgetEntry() {
-    }
+    public BudgetEntry() {}
 
-    public BudgetEntry(Integer lineNumber,
-                       String type,
-                       BigDecimal amount,
-                       String ministry,
-                       String source) {
+    public BudgetEntry(Integer lineNumber, String type, BigDecimal amount, String ministry, String source) {
         this.lineNumber = lineNumber;
         this.type = type;
         this.amount = amount;
@@ -42,47 +30,15 @@ public class BudgetEntry {
         this.source = source;
     }
 
-    public Long getId() {
-        return id;
-    }
-
-    public Integer getLineNumber() {
-        return lineNumber;
-    }
-
-    public void setLineNumber(Integer lineNumber) {
-        this.lineNumber = lineNumber;
-    }
-
-    public String getType() {
-        return type;
-    }
-
-    public void setType(String type) {
-        this.type = type;
-    }
-
-    public BigDecimal getAmount() {
-        return amount;
-    }
-
-    public void setAmount(BigDecimal amount) {
-        this.amount = amount;
-    }
-
-    public String getMinistry() {
-        return ministry;
-    }
-
-    public void setMinistry(String ministry) {
-        this.ministry = ministry;
-    }
-
-    public String getSource() {
-        return source;
-    }
-
-    public void setSource(String source) {
-        this.source = source;
-    }
+    public Long getId() { return id; }
+    public Integer getLineNumber() { return lineNumber; }
+    public void setLineNumber(Integer lineNumber) { this.lineNumber = lineNumber; }
+    public String getType() { return type; }
+    public void setType(String type) { this.type = type; }
+    public BigDecimal getAmount() { return amount; }
+    public void setAmount(BigDecimal amount) { this.amount = amount; }
+    public String getMinistry() { return ministry; }
+    public void setMinistry(String ministry) { this.ministry = ministry; }
+    public String getSource() { return source; }
+    public void setSource(String source) { this.source = source; }
 }
