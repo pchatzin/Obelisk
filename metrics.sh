db14324e3baa759408031476a25db388a608c371
Pinelopi Chatzinikolaou <t8240167@aueb.gr>
2025-12-07 21:13:20 +0200

    metrics.sh updated

--- metrics.sh
+++ metrics.sh
@@ -1,113 +1,30 @@
-#!/usr/bin/env bash
-set -euo pipefail
+#!/bin/bash
 
 echo "===================="
-echo " Git Metrics Report "
+echo " Git Metrics Report"
 echo "===================="
+printf "%-30s | %-8s | %-12s | %-13s\n" "User" "Commits" "Lines Added" "Lines Deleted"
+echo "-------------------------------------------------------------------------------"
 
-# Βεβαιώσου ότι είμαστε σε git repo
-git rev-parse --is-inside-work-tree >/dev/null
+# Get all unique authors across all branches, handle spaces properly
+git log --all --format='%aN' | sort -u | while IFS= read -r author; do
+    # Count commits
+    commits=$(git log --all --author="$author" --pretty=oneline | wc -l)
 
-########################################
-#  Συλλογή authors (emails)
-########################################
-mapfile -t emails < <(git log --all --format='%ae' | sort -u)
+    # Sum lines added/deleted
+    stats=$(git log --all --author="$author" --pretty=tformat: --numstat \
+        | awk '{added+=$1; deleted+=$2} END {print added, deleted}')
 
-# Associative arrays ανά email
-declare -A name_by_email commits added deleted
+    added=$(echo $stats | cut -d' ' -f1)
+    deleted=$(echo $stats | cut -d' ' -f2)
 
-########################################
-#  Όνομα ανά email
-########################################
-for email in "${emails[@]}"; do
-  name=$(
-    git log --all --author="$email" --format='%an' \
-      | sort | uniq -c | sort -rn \
-      | awk '{print substr($0, index($0,$2))}' \
-      | head -1 || true
-  )
-  [[ -z "$name" ]] && name="$email"
+    # Default to 0 if empty
+    [ -z "$added" ] && added=0
+    [ -z "$deleted" ] && deleted=0
 
-  # Ομογενοποίηση ονόματος για την Ελισάβετ
-  if [[ "$name" == "Elisavet" ]]; then
-    name="Elisavet Roumeli"
-  fi
-
-  name_by_email["$email"]="$name"
-done
-
-########################################
-#  Commits ανά email
-########################################
-for email in "${emails[@]}"; do
-  commits["$email"]=$(
-    git log --all --no-merges --author="$email" --pretty=oneline \
-      | wc -l | tr -d ' '
-  )
-done
-
-########################################
-#  Γραμμές added / deleted ανά email
-########################################
-for email in "${emails[@]}"; do
-  read add del < <(
-    git log --all --no-merges --author="$email" --numstat --pretty=tformat: \
-      | awk '
-          { a=$1; d=$2;
-            if (a=="-") a=0;
-            if (d=="-") d=0;
-            add+=a; del+=d
-          }
-          END { print add+0, del+0 }'
-  )
-  added["$email"]="${add:-0}"
-  deleted["$email"]="${del:-0}"
-done
-
-########################################
-#  Άθροιση ανά ΟΝΟΜΑ (όχι email)
-########################################
-
-declare -A total_commits total_added total_deleted
-declare -A seen_name
-names=()
-
-for email in "${emails[@]}"; do
-  name="${name_by_email[$email]:-$email}"
-
-  # (ξανα-ομογενοποίηση για σιγουριά)
-  if [[ "$name" == "Elisavet" ]]; then
-    name="Elisavet Roumeli"
-  fi
-
-  if [[ -z "${seen_name[$name]+x}" ]]; then
-    names+=("$name")
-    seen_name["$name"]=1
-  fi
-
-  total_commits["$name"]=$(( ${total_commits["$name"]:-0} + ${commits["$email"]:-0} ))
-  total_added["$name"]=$(( ${total_added["$name"]:-0} + ${added["$email"]:-0} ))
-  total_deleted["$name"]=$(( ${total_deleted["$name"]:-0} + ${deleted["$email"]:-0} ))
-done
-
-########################################
-#  Τελικό Report
-########################################
-
-printf "\n%-25s | %7s | %10s | %12s\n" \
-  "User" "Commits" "Lines Added" "Lines Deleted"
-echo "--------------------------------------------------------------"
-
-for name in "${names[@]}"; do
-  printf "%-25s | %7s | %10s | %12s\n" \
-    "$name" \
-    "${total_commits["$name"]:-0}" \
-    "${total_added["$name"]:-0}" \
-    "${total_deleted["$name"]:-0}"
+    printf "%-30s | %-8s | %-12s | %-13s\n" "$author" "$commits" "$added" "$deleted"
 done
 
 echo "===================="
-echo " End of Report "
+echo " End of Report"
 echo "===================="
-
-
