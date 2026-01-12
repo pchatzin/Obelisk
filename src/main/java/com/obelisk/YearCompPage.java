148ee85a28f35fefde9619960092443aecc11068
Kornilia Riga <t8240119@aueb.gr>
2026-01-13 01:15:02 +0200

    final changes

--- src/main/java/com/obelisk/YearCompPage.java
+++ src/main/java/com/obelisk/YearCompPage.java
@@ -129,13 +129,13 @@ public class YearCompPage {
         footer.setPadding(new Insets(20, 80, 20, 80));
         footer.setStyle("-fx-background-color: #d5dee2;");
         
-        Button backButton = new Button("Back to Main Menu");
+        Button backButton = new Button("<-Back to Main Menu");
         backButton.setStyle("-fx-font-size: 16px; -fx-background-color: #363636; -fx-text-fill: white;");
         backButton.setOnAction(e -> {
             stage.setScene(mainApp.createSecondScene(stage));
             stage.setFullScreen(true);
         });
-
+        
         footer.getChildren().add(backButton);
         root.setBottom(footer);
 
